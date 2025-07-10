b667d4aadf65c8a9435fa7e6bce202ff6b506d2f
MichaelJLittle <michaelvlittle76@gmail.com>
2025-07-10 13:20:35 -0400

    Add files via upload

---
+++ Connection Tester 3000.ps1
@@ -0,0 +1,39 @@
+﻿# Connection Tester 3,000
+
+$ConnectionFolder = "C:\Users\Mike\Desktop\ConnectionLog"
+
+
+# Make sure the folder exists
+if (-not (Test-Path $ConnectionFolder)) {
+    New-Item -ItemType Directory -Path $ConnectionFolder -Force
+}
+
+
+
+
+
+while($true) { 
+
+$GetDate = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+$ConnectionResults = "$ConnectionFolder\Connectionresults_$GetDate.txt"
+# Log timestamp
+"Test Run: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')" | Out-File -FilePath $ConnectionResults -Append
+
+# Localhost test
+"=== Localhost Test ===" | Out-File $ConnectionResults -Append
+Test-Connection -Count 1 127.0.0.1 | Out-File $ConnectionResults -Append
+
+# Internet tests
+"=== Internet Tests ===" | Out-File $ConnectionResults -Append
+Test-Connection -Count 4 google.com, homedepot.com | Out-File $ConnectionResults -Append
+
+# Extra target
+"Sending test to chatgpt.com" | Out-File $ConnectionResults -Append
+Test-Connection -Count 1 chatgpt.com | Out-File $ConnectionResults -Append
+
+# Open the results
+# Start-Process notepad.exe $ConnectionResults
+
+Start-Sleep -Seconds 300
+
+} 
---
+++ GrabNetworkInfo.ps1
@@ -0,0 +1,15 @@
+﻿$DateString = Get-Date -Format "yyyy-MM-dd"
+
+
+#this is for specifying a date for when our file is made. 
+
+
+$NetInfoLog =  "C:\NetInfo_$DateString.txt" 
+# Create folder if it doesn't exist
+if (-not (Test-Path -Path $NetInfoLog)) {
+    New-Item -ItemType File -Path $NetInfoLog -Force
+    }
+
+Get-NetIPConfiguration -Detailed | Out-File -FilePath $NetInfoLog -Force 
+
+Start-Process notepad.exe $NetInfolog
