5494a2e5c095d644afc5b762ffec35d157c1e2a4
MichaelJLittle <michaelvlittle76@gmail.com>
2025-08-20 15:44:47 -0400

    Add files via upload

---
+++ ClipboardMonitorVer1.ps1
@@ -0,0 +1,26 @@
+﻿ 
+
+ $ClipLog = "C:\CLipLog.txt" # this makes the file, and if it's not there we make it. C drive is easiest to pick 
+
+ If(-not(Test-Path $ClipLog) ) { 
+
+ New-Item -ItemType File -Path ClipLog -Force
+ } 
+
+
+
+ while($true)  { 
+
+ $Date = Get-Date -Format "yyyy-MM-dd HH:mm:ss" 
+ 
+ $ClipBoard = Get-Clipboard  
+
+ Add-Content -Value "The user has *$Clipboard* , as of $Date" -Path $Cliplog
+
+
+ start-sleep 6
+
+
+ } 
+ # i imagine this one is good if you think information is being copied when it shouldn't be, its pretty simple and utlizes that get-clipboard cmdlet. we keep it all in the loop
+ #to keep it updated 
